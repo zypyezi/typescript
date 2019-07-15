583ee73e215d0a308a50b670041034e2a6a62bc6
zhangyp <zhangyp@shinemo.mail.com>
2019-07-15 14:16:28 +0800

    base

--- src/typescript/demo1.js
+++ src/typescript/demo1.js
@@ -1,15 +1,4 @@
-function sayHello(person) {
-
-    var decLiteral = 6;
-    var hexLiteral = 0xf00d;
-    // ES6 中的二进制表示法
-    var binaryLiteral = 10;
-    // ES6 中的八进制表示法
-    var octalLiteral = 484;
-    var notANumber = NaN;
-    var infinityNumber = Infinity;
-    
-    return 'Hello, ' + person;
+function getLength(something) {
+    return something.length;
+    // 
 }
-var user = 'zyp';
-console.log(sayHello(user));
