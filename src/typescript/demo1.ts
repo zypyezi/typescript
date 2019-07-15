59c7505d4f33a2edb09bbaa66adaa05e0b010d8f
zhangyp <zhangyp@shinemo.mail.com>
2019-07-15 09:28:49 +0800

    init

---
+++ src/typescript/demo1.ts
@@ -0,0 +1,14 @@
+function sayHello(person: string) {
+    let decLiteral: number = 6;
+    let hexLiteral: number = 0xf00d;
+    // ES6 中的二进制表示法
+    let binaryLiteral: number = 0b1010;
+    // ES6 中的八进制表示法
+    let octalLiteral: number = 0o744;
+    let notANumber: number = NaN;
+    let infinityNumber: number = Infinity;  
+    return 'Hello, ' + person;
+}
+
+let user = 'zyp';
+console.log(sayHello(user));
