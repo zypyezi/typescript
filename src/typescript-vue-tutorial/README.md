dfbcb0b6966e40dc89f64e2f1653a744853ee2ad
zhangyp <zhangyp@shinemo.mail.com>
2019-07-25 20:01:09 +0800

    typescript vue demo

---
+++ src/typescript-vue-tutorial/README.md
@@ -0,0 +1,28 @@
+
+## typescript + vue
+
+```
+    demo1 : 直接使用 ts文件
+    
+    demo2 : 使用.vue 文件的形式
+        1. script 标签上需要加 lang = "ts" 表明使用typescript语法
+        2. import文件时， 默认拓展名为ts， 所以需要显式表明 .vue文件
+        3. export default Vue.extend({ })
+        4. 需要添加vue-shims.d.ts 文件去告知typescript 如何去处理.vue文件
+
+    demo3 ： 使用修饰符 .vue 文件
+        1. 使用到vue-property-decorator 包， 【vue-class-component 是vue-property-decorator 里被引入的依赖】
+        2. js部分 语法差异较大， 详见下方说明 
+        3. tsconfig.json中需要设置 experimentalDecorators 为true
+
+```
+
+
+
+
+
+```
+    vue-property-decorator 详细使用方法：
+
+
+```
--- src/typescript-vue-tutorial/src/index.ts
+++ src/typescript-vue-tutorial/src/index.ts
@@ -1,15 +1,27 @@
 // src/index.ts
 
 import Vue from "vue";
-
+import Demo1 from './components/demo1'
+import Demo2 from './components/demo2.vue'
+import Demo3 from './components/demo3.vue'
 let v = new Vue({
     el: "#app",
     template: `
     <div>
         <div>Hello {{name}}!</div>
-        Name: <input v-model="name" type="text">
+        <div>demo1:</div>
+        <demo1 :name="name" :initialEnthusiasm="5"></demo1>
+        <div>demo2:</div>
+        <demo2 :name="name" :initialEnthusiasm="5"></demo2>
+        <div>demo3:</div>
+        <demo3 :name="name" :initialEnthusiasm="5"></demo3>
     </div>`,
     data: {
         name: "World"
+    },
+    components: {
+        Demo1,
+        Demo2,
+        Demo3
     }
 });
